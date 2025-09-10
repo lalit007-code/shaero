beaf5c30f916e1efdd873e5c6b82c9db63ba189a
Lalit Singh <lalit284546@gmail.com>
2025-09-10 10:44:55 +0530

    create user mutation function

---
+++ convex/user.ts
@@ -0,0 +1,34 @@
+import { v } from "convex/values";
+import { mutation } from "./_generated/server";
+
+export const user = mutation({
+  args: {
+    username: v.string(),
+    fullname: v.string(),
+    image: v.string(),
+    bio: v.optional(v.string()),
+    email: v.string(),
+    clerkId: v.string(),
+  },
+  handler: async (ctx, args): Promise<any> => {
+    // if the user existed or not
+    const existingUser = await ctx.db
+      .query("users")
+      .withIndex("by_clerk_id", (q) => q.eq("clerkId", args.clerkId))
+      .first();
+
+    if (existingUser) return user;
+    // create user in database
+    await ctx.db.insert("users", {
+      username: args.username,
+      fullname: args.fullname,
+      image: args.image,
+      bio: args.bio,
+      email: args.email,
+      clerkId: args.clerkId,
+      followers: 0,
+      following: 0,
+      posts: 0,
+    });
+  },
+});
