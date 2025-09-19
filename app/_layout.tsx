67a0729b106c0d0ffab1f763a3cd0918331c6961
Lalit Singh <lalit284546@gmail.com>
2025-09-19 12:25:46 +0530

    added layout, might later debug agaiin

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,23 @@
 import InitialLayout from "@/components/InitialLayout";
 import ClerkAndConvexProvider from "@/providers/ClerkAndConvexProvider";
+import { useFonts } from "expo-font";
+import { SplashScreen } from "expo-router";
+import { useCallback } from "react";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
+// SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
+  const [fontLoaded] = useFonts({
+    "JetBrainsMono-Medium": require("@/assets/fonts/JetBrainsMono-Medium.ttf"),
+  });
+
+  const onLayoutView = useCallback(async () => {
+    if (fontLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontLoaded]);
+
+  // onLayoutView();
   return (
     <ClerkAndConvexProvider>
       <SafeAreaProvider>
